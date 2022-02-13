4c797fe48b09a7e51297c2ad45ede4d7144e46ea
dmoggles <dmitry.mogilevsky@gmail.com>
2022-02-13 12:34:31 +0000

    Adding basic examples notebook (#18)

--- src/footmav/__init__.py
+++ src/footmav/__init__.py
@@ -6,8 +6,8 @@ __all__ = ["__version__"]
 from footmav.operations.filter import filter
 from footmav.operations import filter_objects as filters
 from footmav.operations.filter_objects import Filter
-from footmav.operations.aggregations import aggregate_by
+from footmav.operations.aggregations import aggregate_by, rank
 from footmav.operations.normalize import per_90
-from footmav.data_definitions.fbref import fbref_columns as fc
+from footmav.data_definitions.fbref import fbref_columns as fb
 from footmav.odm.fbref_data import FbRefData
 from footmav.odm.data import Data
