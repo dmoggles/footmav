871e9222b1b2f3bfa10dbaf758ec8f3e94e74b51
Dmitry Mogilevsky <dmitry.mogilevsky@gmail.com>
2022-03-07 10:20:09 +0000

    adding zscore

--- src/footmav/__init__.py
+++ src/footmav/__init__.py
@@ -7,7 +7,7 @@ from footmav.operations.filter import filter
 from footmav.operations import filter_objects as filters
 from footmav.operations.filter_objects import Filter
 from footmav.operations.aggregations import aggregate_by, rank
-from footmav.operations.normalize import per_90
+from footmav.operations.normalize import per_90, z_score
 from footmav.data_definitions.fbref import fbref_columns as fb
 from footmav.data_definitions.understat import understat_columns as us
 from footmav.odm.fbref_data import FbRefData
